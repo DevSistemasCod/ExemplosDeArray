d22fede6369dce004c9cc68008a38c25f695da28
DevSistemasCod <124713140+DevSistemasCod@users.noreply.github.com>
2023-03-18 20:33:45 -0300

    Update InicializarArray.java

--- InicializarArray.java
+++ InicializarArray.java
@@ -1,6 +1,6 @@
 package arrays;
 
-public class InicializarArray {
+public class ExemploInicializarArray {
 
 	public static void main(String[] args) {
 		int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
